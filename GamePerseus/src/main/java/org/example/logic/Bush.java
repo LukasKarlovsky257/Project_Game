d49af56fa6660b36838feed7bbb53e394e2ec319
LukasKarlovsky257 <163822206+LukasKarlovsky257@users.noreply.github.com>
2024-05-26 15:43:51 +0200

    Add files via upload

---
+++ GamePerseus/src/main/java/org/example/logic/Bush.java
@@ -0,0 +1,54 @@
+package org.example.logic;
+
+import java.awt.*;
+
+public class Bush extends Entity{
+    private int x1;
+    private int y1;
+    private String url;
+    private boolean active;
+    private Coords coordStart;
+    private Coords coordEnd;
+
+    public Bush(int x1, int y1, String url) {
+        super(x1, y1, url);
+        this.coordStart = new Coords(x1, y1);
+        this.coordEnd = new Coords(x1, y1);
+    }
+    public void activate() {
+        this.active = true;
+    }
+
+    public Coords getCoordStart() {
+        return coordStart;
+    }
+
+    public Coords getCoordEnd() {
+        return coordEnd;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+}
